3c0d06e3f71a78c07f641ea1b19d1dc2858f1473
tush87 <tusharj8788@gmail.com>
2023-05-24 19:56:45 +0530

    add user create session and add jsonwebtoken

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,4 +1,5 @@
 const UserDB = require('../models/user');
+const jsonwebtoken = require('jsonwebtoken');
 
 //create User
 module.exports.createUser = async function(req,res){
@@ -23,6 +24,46 @@ module.exports.createUser = async function(req,res){
         //if user already exist
         return res.status(401).json({
             message: "User aready exist",
+        })
+    }
+    catch(err){
+        console.log(err);
+
+        let statusCode = 500, message = "Internal Server Error";
+        //field missing error
+        if (err.name == "ReferenceError") {
+            statusCode = 403;
+            message = "field missing error";
+        }
+        //field missmatch
+        else if (err.name == "TypeError") {
+            statusCode = 403;
+            message = "field missmatch"
+        }
+
+        return res.status(statusCode).json({
+            message: message
+        })
+    }
+}
+
+//create session 
+module.exports.createSession=async function(req,res){
+    try{
+        //find user in db
+        let user = await UserDB.findOne({email:req.body.email});
+        //user not found and password missmactch 
+        if(!user || user.password != req.body.password){
+            return res.status(402).json({
+                message:"email or password not match"
+            });
+        }
+
+        //return token
+        return res.status(200).json({
+            message:"user successfully login",
+            //genrate token
+            Token:jsonwebtoken.sign(user.toJSON(),"secret",{expiresIn:1000*60*100})
         })
     }
     catch(err){
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -5,4 +5,7 @@ const router = express.Router();
 // create user
 router.post('/create',userController.createUser);
 
+//create session
+router.post('/create-session',userController.createSession);
+
 module.exports = router;
