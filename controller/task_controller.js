cc777b84c1258d9a800dbc3eb38a7d6db6b909ca
tush87 <tusharj8788@gmail.com>
2023-05-24 20:28:55 +0530

    add create task

--- controller/task_controller.js
+++ controller/task_controller.js
@@ -1,5 +1,6 @@
 const TaskDB = require('../models/task');
 
+//all task
 module.exports.getAllTasks = async function (req, res) {
     try {
         //find all task
@@ -15,6 +16,40 @@ module.exports.getAllTasks = async function (req, res) {
 
         return res.status(400).json({
             message: "error"
+        })
+    }
+}
+
+//create new task
+module.exports.createTask = async function (req, res) {
+    try {
+       
+        let { title, description, status } = req.body;
+        //create new task
+        let Task = await TaskDB.create({
+            title: title,
+            description: description,
+            status: status,
+            user: req.user.id
+        });
+
+        return res.status(201).json({
+            message: "New Task Created",
+            Task
+        })
+    }
+    catch (err) {
+        console.log(err);
+
+        let statusCode = 500, message = "Internal Server Error";
+        // field validation or field missing error
+        if (err.name == "ValidationError") {
+            statusCode = 403;
+            message = "field validation or field missing error";
+        }
+
+        return res.status(statusCode).json({
+            message: message
         })
     }
 }
--- routes/v1/task.js
+++ routes/v1/task.js
@@ -3,6 +3,10 @@ const taskController = require('../../controller/task_controller');
 const passport = require('passport');
 const router = express.Router();
 
+//all task
 router.get('/',passport.authenticate('jwt',{session:false}),taskController.getAllTasks);
+
+//create task
+router.post('/create',passport.authenticate('jwt',{session:false}),taskController.createTask);
 
 module.exports = router;
