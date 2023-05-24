b9a385501dc6cb154e6ac473191df434686e0d90
tush87 <tusharj8788@gmail.com>
2023-05-24 19:51:16 +0530

    add create user

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,8 +1,47 @@
-module.exports.create =async function(req,res){
+const UserDB = require('../models/user');
+
+//create User
+module.exports.createUser = async function(req,res){
     try{
-        console.log(req.body);
+        //password or confirmpassword not match
+        if(req.body.password != req.body.confirmpassword){
+            return res.status(401).json({
+                message: "Password not match",
+            })    
+        }
+
+        //check user already exist or not 
+        let user = await UserDB.findOne({email:req.body.email});
+
+        if(!user){
+            // create new user
+            user = await UserDB.create(req.body);
+            return res.status(201).json({
+                message: "User Create Successfuly",
+            })
+        }
+        //if user already exist
+        return res.status(401).json({
+            message: "User aready exist",
+        })
     }
     catch(err){
+        console.log(err);
 
+        let statusCode = 500, message = "Internal Server Error";
+        //field missing error
+        if (err.name == "ReferenceError") {
+            statusCode = 403;
+            message = "field missing error";
+        }
+        //field missmatch
+        else if (err.name == "TypeError") {
+            statusCode = 403;
+            message = "field missmatch"
+        }
+
+        return res.status(statusCode).json({
+            message: message
+        })
     }
 }
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -3,6 +3,6 @@ const userController = require('../../controller/user_controller');
 const router = express.Router();
 
 // create user
-router.post('/create',userController.create);
+router.post('/create',userController.createUser);
 
 module.exports = router;
